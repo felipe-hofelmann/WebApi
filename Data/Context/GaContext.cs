e27f5efa8c31d20b6d9309227f5b702ebb3ae7da
Felipe Hofelmann <felipehofelmann21@gmail.com>
2021-08-20 09:51:13 -0300

    Create Folder Context With class GaContext

---
+++ Data/Context/GaContext.cs
@@ -0,0 +1,19 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    class GaContext : DbContext
+    {
+        public DbSet<Garrafa> Garrafa { get; set; }
+        public GaContext():base("Data Source=192.168.0.114;Initial Catalog=Felipe;Persist Security Info=True;User ID=Felipe;Password=#felipe")
+        {
+
+        }
+    }
+}
