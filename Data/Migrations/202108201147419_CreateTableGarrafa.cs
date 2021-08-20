0d78467a107965121caa2da5680b0efc09844311
Felipe Hofelmann <felipehofelmann21@gmail.com>
2021-08-20 09:51:52 -0300

    Create Migrations

---
+++ Data/Migrations/202108201147419_CreateTableGarrafa.cs
@@ -0,0 +1,27 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableGarrafa : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Garrafas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Bebida = c.String(),
+                        ML = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Garrafas");
+        }
+    }
+}
